b28c3a52cb615d8a5892133d20b19febc5a8196c
SumitM1234 <166917660+SumitM1234@users.noreply.github.com>
2024-09-10 19:08:22 +0530

    Create Missingnum.cpp

---
+++ Missingnum.cpp
@@ -0,0 +1,9 @@
+int missingNumber(vector<int>&a, int N) {
+    int expected_sum = ( N * (N+1)) / 2;
+    int actual_sum = 0;
+    for(int i = 0 ;i < N-1 ; i++) {
+        actual_sum += a[i];
+    }
+    int missingNum = expected_sum - actual_sum;
+    return missingNum;
+}
